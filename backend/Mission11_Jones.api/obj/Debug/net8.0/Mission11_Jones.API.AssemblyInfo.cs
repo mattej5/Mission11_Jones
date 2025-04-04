7b05885c7b33889503221299ba4cbffdbd809d8e
mattej5 <matthew.jones6288@gmail.com>
2025-04-03 20:44:57 -0600

    Commit made from VS Code Azure Static Web Apps

--- backend/Mission11_Jones.api/obj/Debug/net8.0/Mission11_Jones.API.AssemblyInfo.cs
+++ backend/Mission11_Jones.api/obj/Debug/net8.0/Mission11_Jones.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11_Jones.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+542fcebcb798c917a5bd755add239d6d0a0bf6c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b2016290c670980bb47c626e8083654b67ddd9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11_Jones.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11_Jones.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
